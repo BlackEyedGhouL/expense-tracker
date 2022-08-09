0e212268f2e3e1f5052515d2eb154bf651712fc5
BlackEyedGhouL <34senith@gmail.com>
2022-08-09 15:43:05 +0530

    Update app name and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import "../assets/Header.css";
 const Header = (props) => {
   return (
     <div className="header-containter">
-      <h1 className="site-name">Expense Tracker 2022</h1>
+      <h1 className="site-name">Expense Tracker</h1>
       <div className="header-right">
         <h2 className="total-text">Total:</h2>
         <TotalAmount items={props.items} />
